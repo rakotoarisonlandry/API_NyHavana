7a4a36eca44b15f5fd85d9e8eeee05187c1fb361
rakotorisonlandry <rakotorisonlandry@gmail.com>
2023-11-20 09:20:06 +0100

    new commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,23 +12,25 @@ export const register = (req, res) => {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
-
     const q = "INSERT INTO user(`username`,`email`,`password`) VALUES (?)";
     const values = [req.body.username, req.body.email, hash];
 
     db.query(q, [values], (err, data) => {
       if (err) return res.json(err);
       return res.status(200).json("User has been create.");
-    }); 
+    });
   });
 };
 export const login = (req, res) => {
-
   //Chek User
-  const q = "SELECT * FROM user WHERE username = ?"
-  db.query(q,[req,body,username], (err,data) => {
-    if (err) return res.json(err)
-    if(data.length === 0 ) return res.status(404).json("user Not found !")
-  })
+  const q = "SELECT * FROM user WHERE username = ?";
+  db.query(q, [req, body, username], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length === 0) return res.status(404).json("user Not found !");
+
+    //Check password
+    const IsPassordCorrect = bcrypt.compareSync(req.bode.password,data[0].password)
+    if(!IsPassordCorrect)return res.status(400).json("wrong passord or username")
+  });
 };
 export const logout = (req, res) => {};
