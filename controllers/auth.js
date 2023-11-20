7cf71894aec0b6867ce96109ba4c8c5b56596c69
rakotorisonlandry <rakotorisonlandry@gmail.com>
2023-11-20 09:15:11 +0100

    new commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,16 +10,25 @@ export const register = (req, res) => {
 
     //Hash the password   and create a user
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.genHashSync(req.body.password, salt);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
-    const q = "INSERT INTO user(`username`,`email `,`password`) VALUES (?)";
+
+    const q = "INSERT INTO user(`username`,`email`,`password`) VALUES (?)";
     const values = [req.body.username, req.body.email, hash];
 
     db.query(q, [values], (err, data) => {
       if (err) return res.json(err);
       return res.status(200).json("User has been create.");
-    });
+    }); 
   });
 };
-export const login = (req, res) => {};
+export const login = (req, res) => {
+
+  //Chek User
+  const q = "SELECT * FROM user WHERE username = ?"
+  db.query(q,[req,body,username], (err,data) => {
+    if (err) return res.json(err)
+    if(data.length === 0 ) return res.status(404).json("user Not found !")
+  })
+};
 export const logout = (req, res) => {};
