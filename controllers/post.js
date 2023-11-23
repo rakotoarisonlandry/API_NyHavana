42912837e8773e1a51541e4def2f3a35fbd402f9
rakotorisonlandry <rakotorisonlandry@gmail.com>
2023-11-23 14:10:13 +0100

    new commit

--- controllers/post.js
+++ controllers/post.js
@@ -1,44 +1,88 @@
 import { db } from "../db.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 export const getPosts = (req, res) => {
   const q = req.query.cat
     ? "SELECT * FROM posts WHERE cat=?"
     : "SELECT * FROM posts";
 
-    db.query(q,[req.query.cat],(err,data) => {
-      if(err) return res.send(err)
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.send(err);
 
-      return res.status(200).json(data)
-    })
+    return res.status(200).json(data);
+  });
 };
-
 
 export const getPost = (req, res) => {
-  const q = "SELECT `username` , `title` ,`desc`, p.img,u.img AS userImg ,`cat` , `date` FROM user u JOIN posts p ON u.id= p.userId WHERE p.id = ? "
-  db.query(q,[req.params.id], (err,data) =>{
-    if(err) return res.status(500).json(err)
-    return res.status(200).json(data[0]) 
-  })
+  const q =
+    "SELECT p.id , `username` , `title` ,`desc`, p.img,u.img AS userImg ,`cat` , `date` FROM user u JOIN posts p ON u.id= p.userId WHERE p.id = ? ";
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(data[0]);
+  });
 };
 export const addPost = (req, res) => {
-  res.json("from controller");
+  const token = req.cookies.access_token;
+  if (!token) returnres.status(401).json("Not authenticated!");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q =
+      "INSERT INTO posts(`title`,desc`,`img`, `cat` ,`date`,`userId`) VALUES (?)";
+
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.cat,
+      req.body.date,
+      userInfo.id,
+    ];
+
+    db.query(q, [values],(err,data) =>{
+      if (err) return res.status(500).json(err);
+      return res.json("Post has been created ! ")
+    })
+  });
 };
 export const deletePost = (req, res) => {
-  const token =req.cookies.access_token
-  if(!token) returnres.status(401).json("Not authenticated!")
+  const token = req.cookies.access_token;
+  if (!token) returnres.status(401).json("Not authenticated!");
 
-  jwt.verify(token, "jwtkey", (err,userInfo) => {
-    if(err) return res.status(403).json("Token is not valid!")
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
 
-    const postId = req.params.id
-    const q = "DELETE FROM posts WHERE `id`= ? AND `userId`= ?  " 
-    db.query(q,[postId,userInfo.id], (err,data) =>{
-      if(err) return res.status(403).json("You can delete only your post")
-      
-      return res.json("post has been deleted!")
-    })
-  })
+    const postId = req.params.id;
+    const q = "DELETE FROM posts WHERE `id`= ? AND `userId`= ?  ";
+    db.query(q, [postId, userInfo.id], (err, data) => {
+      if (err) return res.status(403).json("You can delete only your post");
+
+      return res.json("post has been deleted!");
+    });
+  });
 };
 export const updatePost = (req, res) => {
-  res.json("from controller");
+  const token = req.cookies.access_token;
+  if (!token) returnres.status(401).json("Not authenticated!");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+const postId = req.params.id
+
+    const q =
+      "UPDATE posts SET `title`=? ,`desc`=?, `img`=?, `cat`=? WHERE `id`=? AND `userID`=?";
+
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.cat,
+    ];
+
+    db.query(q, [...values,postId,userInfo.id],(err,data) =>{
+      if (err) return res.status(500).json(err);
+      return res.json("Post has been Updated! ")
+    })
+  });
 };
--- index.js
+++ index.js
@@ -4,12 +4,31 @@ import cors from "cors";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import muter from "multer";
+import multer from "multer";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../Front/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now()+file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file
+  res.status(200).json(file.filename);
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
